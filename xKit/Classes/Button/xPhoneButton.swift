4b57c3a0cea87d54aa2457d73badf5e3153663cc
kaibuleite <177955297@qq.com>
2025-02-11 16:26:08 +0800

    拨打电话Button，Label

---
+++ xKit/Classes/Button/xPhoneButton.swift
@@ -0,0 +1,20 @@
+//
+//  xPhoneButton.swift
+//  xKit
+//
+//  Created by Mac on 2025/2/11.
+//
+
+import UIKit
+
+class xPhoneButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
